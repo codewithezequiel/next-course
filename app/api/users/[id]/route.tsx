ccd847417f71009b48c7e9a597c957a51efcf46e
Ezequiel Novoa <ezequiel.novoa3@gmail.com>
2023-09-25 15:06:38 -0700

    CRUD operations with prisma client

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,16 +1,20 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "../schema";
+import { prisma } from "../../../../prisma/client";
 
 interface Props {
-  params: { id: number };
+  params: { id: string };
 }
-export function GET(request: NextRequest, { params }: Props) {
+export async function GET(request: NextRequest, { params }: Props) {
+  const user = await prisma.user.findUnique({
+    where: { id: parseInt(params.id) },
+  });
   // Fetch data from db
   // if not found, return 404 error
-  if (params.id > 10)
+  if (!user)
     return NextResponse.json({ error: "User not found" }, { status: 404 });
 
-  return NextResponse.json({ id: 1, name: "Zeke" });
+  return NextResponse.json(user);
 }
 
 export async function PUT(request: NextRequest, { params }: Props) {
@@ -24,24 +28,37 @@ export async function PUT(request: NextRequest, { params }: Props) {
     return NextResponse.json(validation.error.errors, { status: 400 });
   // fetch the user with the given id
   // if doesnt exist, return 404,
-  if (params.id > 10)
+  const user = await prisma.user.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+  if (!user)
     return NextResponse.json({ error: "User not found" }, { status: 404 });
   // update user,
+  const updatedUser = await prisma.user.update({
+    where: { id: user.id },
+    data: {
+      name: body.name,
+      email: body.email,
+    },
+  });
   // return updated user
-  return NextResponse.json({ id: 1, name: body.name });
+  return NextResponse.json(updatedUser);
 }
 
 export async function DELETE(request: NextRequest, { params }: Props) {
-  const body = await request.json();
-  // validate the request body,
-  // if invalid, return 400,
-  if (!body.name)
-    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+  
+  const user = await prisma.user.findUnique({
+    where: { id: parseInt(params.id) },
+  });
 
   // fetch the user with the given id
   // if doesnt exist, return 404,
-  if (params.id > 10)
+  if (!user)
     return NextResponse.json({ error: "User not found" }, { status: 404 });
+
+  await prisma.user.delete({
+    where: { id: user.id },
+  });
 
   return NextResponse.json({});
 }
--- prisma/client.ts
+++ prisma/client.ts
@@ -1,5 +1,22 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
 
-export default prisma;
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+export const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
+
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+// Any time we create or change a model and then create a migration, Prisma CLI
+// regenerates this Prisma Client so that it is always in sync with our models.
+// Best practice to make sure there is a single instance of Prisma Client running in the
+// app.
